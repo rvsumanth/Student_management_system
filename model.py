7e1f5409ba9e6a841d503ae52ee5dc3459e1f281
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-07-13 12:17:46 +0530

    Updated Internal code - Implemented JSON based CRUD operations

--- model.py
+++ model.py
@@ -1,13 +1,19 @@
 import json
+from utils import *
 from Custom_Exceptions import *
 
 class Student:
-    def __init__(self, name: str, age: int,dob: str, phone: int, address: str, roll: int, branch: str, fees: int):
+    def __init__(self, name: str, age: int, dob: str, phone: int, state: str, 
+                 district: str, city: str, pincode: int, roll: int, branch: str, 
+                 fees: int):
         self._name = name
         self._age = age
         self._dob = dob
         self._phone = phone
-        self._address = address
+        self._state = state
+        self._district = district
+        self._city = city
+        self._pincode = pincode
         self._roll = roll
         self._branch = branch
         self._fees = fees
@@ -25,11 +31,17 @@ class Student:
             raise StudentAlreadyExisted()
         else:
             all_data[student_key] = {
+                'roll number': self._roll,
                 'name': self._name,
                 'age': self._age,
                 'dob': self._dob,
                 'phone': self._phone,
-                'address': self._address,
+                'Address':{
+                            'state': self._state,
+                            'district': self._district,
+                            'city': self._city,
+                            'pincode': self._pincode,
+                        },
                 'branch': self._branch,
                 'fees': self._fees
             }
@@ -37,11 +49,11 @@ class Student:
                 json.dump(all_data, f, indent = 4)
         
 
-    @property
-    def student_details(self, id: int) -> dict:
+    
+    def get_student(self, id: int) -> dict:
        try:
             with open('data.json', 'r') as f:
-              data = json.dump(f)
+              data = json.load(f)
         
        except (FileNotFoundError, json.JSONDecodeError) as e:
            print(f'Error: {e}')
@@ -49,47 +61,47 @@ class Student:
        else:
            if str(id) in data:
                return data[str(id)]
+           else:
+               raise StudentNotFound()
            
+           
+    def update_student(self, id):
+        try: 
+            with open('data.json' , 'r') as f:
+                data = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print(f'Error: {e}')
+        
+        if str(id) in data:
+            collect_info = collect_student_details(id)
+            with open('data.json', 'w') as f:
+                data[str(id)] = collect_info
+                json.dump(data, f, indent= 4)
+        else:
+            raise StudentNotFound()
+        
+    def delete_student(self, id) -> bool:
+        try:
+            with open('data.json' , 'r') as f:
+                data = json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            print(f'Error: {e}')
+        
+        if str(id) in data:
+            del data[str(id)]
+            with open('data.json', 'w') as f:
+                json.dump(data, f, indent = 4)
+            return True
+        else:
+            raise StudentNotFound()
+        
+        return False
+        
         
 
 
-    @student_details.setter
-    def student_details(self,name: str, age: int, phone: int, address: str, roll: str, branch: str, fees: int):
-        if name:
-            self._name = name
-        else:
-            raise ValueError('Invalid Name')
-        
-        if age < 18:
-            raise ValueError('Invalid Age')
-        else:
-            self._age = age
-
-        if phone:
-            self._phone = phone
-        else:
-            raise ValueError('Invalid phone')
-        
-        if address:
-            self._address = address
-        else:
-            raise ValueError('Invalid address')
-        
-        if roll: 
-            self._roll = roll
-        else:
-            raise ValueError('Invalid roll number')
-        
-        if branch:
-            self._branch = branch
-        else:
-            raise ValueError('Invalid branch')
-        
-        if fees < 0:
-            raise ValueError('Invalid Amount')
-        else:
-            self._fees = fees
 
 
-    def remove_student(self, roll):
-        if roll
+
+
+
