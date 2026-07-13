daff843e1c95b5a46efdb3d58ee73516b009bb7b
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-07-13 12:15:47 +0530

    Added New Custom Exception

--- Custom_Exceptions.py
+++ Custom_Exceptions.py
@@ -5,9 +5,13 @@ Exceptions
 '''
 
 class StudentNotFound(Exception):
-    def __init__(self):
-        super().__init__('Student Not Found')
+    def __init__(self, *args):
+        super().__init__('No Student is existed with that roll number')
 
 class StudentAlreadyExisted(Exception):
     def __init__(self, *args):
         super().__init__('Student Already Existed')
+
+class StudentAgeInvalid(Exception):
+    def __init__(self, *args):
+        super().__init__('Student Age must be greater than 18')
