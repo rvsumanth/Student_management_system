86746d4806a7168804a6271fec54d974c89cace2
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-07-15 22:04:06 +0530

    Added credentials collecting function and login fucntion

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from Custom_Exceptions import *
+import json
 
 def collect_student_details(id) -> dict:
     name = input('Enter name: ')
@@ -28,3 +29,35 @@ def collect_student_details(id) -> dict:
                 'branch': branch,
                 'fees': fees
             }
+
+def collect_cred_data() -> dict:
+    role = input('Enter role: ')
+    username = input('Enter username: ')
+    password = input('Enter Password: ')
+    if role and  username and password is not None:
+        return {
+            'role': role,
+            'username': username,
+            'password': password
+        }
+    else:
+        raise ValueError('Null Values are not accepted')
+     
+def login(id: int, username: str, password: str) -> bool:
+    try:
+        with open('user_data.json', 'r') as f:
+            all_data = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return False
+    
+    if str(id) in all_data:
+        if all_data[str(id)]['username'] == username:
+            if all_data[str(id)]['password'] == password:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else: 
+        return False
+    
